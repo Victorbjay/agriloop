dd8b39b35971ee4bf843de8d88e4ce27978a6f59
Victor Okpe <okpevictor84@gmail.com>
2026-03-23 19:07:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9546342852-2fa34",
+  "appId": "1:708269562234:web:c77d3130c37782ef8ddec3",
+  "apiKey": "AIzaSyALpjhoqKe0Xm7Nq9T0-KSoUewpv9NrjbU",
+  "authDomain": "studio-9546342852-2fa34.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "708269562234"
+};
